00e05e212bd2d0bb3e365494d5a1e9cb6b0e2511
Andrew Smith <andrewchristophersmith@gmail.com>
2015-09-18 10:02:36 -0700

    Added formant extraction

--- src/lib.rs
+++ src/lib.rs
@@ -95,6 +95,100 @@ impl<T> Resonance<T> for Vec<Complex<T>> where T: Float + FromPrimitive {
     }
 }
 
+pub struct FormantFrame<T: Float> {
+    frequency: T,
+}
+
+pub struct FormantExtractor<'a, T: 'a + Float> {
+    num_formants: usize,
+    frame_index: usize,
+    resonances: &'a Vec<Vec<T>>,
+    estimates: Vec<T>
+}
+
+impl<'a, T: 'a + Float + PartialEq> FormantExtractor<'a, T> {
+    pub fn new(
+        num_formants: usize, 
+        resonances: &'a Vec<Vec<T>>, 
+        starting_estimates: Vec<T>) -> Self {
+        FormantExtractor { 
+            num_formants: num_formants, 
+            resonances: resonances, 
+            frame_index: 0,
+            estimates: starting_estimates 
+        }
+    }
+}
+
+impl<'a, T: 'a + Float + PartialEq + FromPrimitive> Iterator for FormantExtractor<'a, T> {
+    type Item = Vec<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let frame = self.resonances[self.frame_index].clone();
+        let mut slots: Vec<Option<T>> = self.estimates.iter()
+        .enumerate()
+        .map(|(index, estimate)| {
+            let mut indices: Vec<usize> = (0..frame.len()).collect();
+            indices.sort_by(|a, b| {
+                (frame[*a] - *estimate).abs().partial_cmp(&(frame[*b] - *estimate).abs()).unwrap()
+            });
+            let win = indices.first().unwrap().clone();
+            Some(frame[win]) // (resonance_index, resonance_freq)
+        }).collect();
+
+        // Step 3: Remove duplicates. If the same peak p_j fills more than one slots S_i keep it
+        // only in the slot S_k which corresponds to the estimate EST_k that it is closest to in
+        // frequency, and remove it from any other slots.
+        let mut w: usize = 0;
+        let mut has_unassigned: bool = false;
+        for r in (1..slots.len()) {
+            match slots[r] {
+                Some(v) => { 
+                    if v == slots[w].unwrap() {
+                        if (v - self.estimates[r]).abs() < (v - self.estimates[w]).abs() {
+                            slots[w] = None;
+                            has_unassigned = true;
+                            w = r;
+                        } else {
+                            slots[r] = None;
+                            has_unassigned = true;
+                        }
+                    } else {
+                        w = r;
+                    }
+                },
+                None => { }
+            }
+        }
+
+        if has_unassigned {
+            // Step 4: Deal with unassigned peaks. If there are no unassigned peaks p_j, go to Step 5.
+            // Otherwise, try to fill empty slots with peaks not assigned in Step 2 as follows.
+            for j in 0..frame.len() {
+                let peak = Some(frame[0]);
+                if slots.contains(&peak) { continue; }
+                match slots[j] {
+                    Some(_) => { },
+                    None => { slots[j] = peak; continue; }
+                }
+                match slots[j-1] {
+                    Some(_) => { },
+                    None => { slots.swap(j, j-1); slots[j] = peak; continue; }
+                }
+                match slots[j+1] {
+                    Some(_) => { },
+                    None => { slots.swap(j, j+1); slots[j] = peak; continue; }
+                }
+            }
+        }
+
+        let winners: Vec<T> = slots.iter().map(|v| v.unwrap_or(T::from_f32(0.0).unwrap())).collect();
+        self.estimates = winners.clone();
+        self.frame_index += 1;
+        Some(winners)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*; 
@@ -131,6 +225,33 @@ mod tests {
     fn test_ac() { 
         let sine = Vec::<f64>::sine(16);
         sine.autocorrelate(16);
+    }
+
+    #[test]
+    fn test_formant_extractor() {
+        let resonances = vec![
+            vec![100.0, 150.0, 200.0, 240.0, 300.0], 
+            vec![110.0, 180.0, 210.0, 230.0, 310.0],
+            vec![200.0, 270.0, 290.0, 350.0, 360.0]
+        ];
+        let mut extractor = FormantExtractor::new( 3, &resonances, vec![140.0, 230.0, 320.0]);
+        // First cycle has initial guesses
+        match extractor.next() {
+            Some(r) => { assert_eq!(r, vec![150.0, 240.0, 300.0]) },
+            None => { panic!() }
+        };
+
+        // Second cycle should be different
+        match extractor.next() {
+            Some(r) => { assert_eq!(r, vec![180.0, 230.0, 310.0]) },
+            None => { panic!() }
+        };
+
+        // Third cycle should have removed duplicates
+        match extractor.next() {
+            Some(r) => { assert_eq!(r, vec![200.0, 0.0, 290.0]) },
+            None => { panic!() }
+        };
     }
 }
 
