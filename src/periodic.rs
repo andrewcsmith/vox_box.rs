75d378e85531a3c4b46cab118c1cbf78b58765e9
Andrew Smith <andrewchristophersmith@gmail.com>
2016-06-09 11:16:41 -0700

    First rewrite using the sample crate

--- src/periodic.rs
+++ src/periodic.rs
@@ -1,20 +1,38 @@
 extern crate num;
 
-use num::{Float, FromPrimitive, Zero};
+use num::{Float, ToPrimitive, FromPrimitive};
+use super::waves::Normalize;
 
-use std::ops::*;
-use std::cmp::PartialOrd;
-use std::marker::Sized;
+use sample;
+use sample::window::Window;
+use sample::{Sample, ToSampleSlice};
 
-use super::waves::{WindowType, Window, Normalize, HasLength};
 
-#[inline]
-pub fn max_value<T: Float>(data: &[T]) -> T {
-    data.iter().fold(T::nan(), |acc, x| x.max(acc))
+pub trait LagType: sample::window::Type {
+    type Lag: sample::window::Type;
 }
 
-/// Trait for things that can Autocorrelate. Implement the mutable version, which takes a slice of
-/// coefficients, and receive a version that allocates its own vector for free.
+pub struct HanningLag;
+
+impl sample::window::Type for HanningLag {
+    fn at_phase<S: Sample>(phase: S) -> S {
+        let pi_2 = <S::Float as sample::Float>::pi() * 2.0.to_sample();
+        let v = phase.to_float_sample() * pi_2;
+        let one_third: S::Float = (1.0 / 3.0).to_sample();
+        let two_thirds: S::Float = (2.0 / 3.0).to_sample();
+        let one: S::Float = 1.0.to_sample();
+        ((one - phase.to_float_sample()) * (two_thirds + (one_third * sample::Float::cos(v))) 
+            + (one / pi_2) * sample::Float::sin(v)).to_sample::<S>()
+    }
+}
+
+impl LagType for sample::window::Hanning {
+    type Lag = HanningLag;
+}
+
+/// Trait for things that can Autocorrelate. Implement the mutable version,
+/// which takes a slice of coefficients, and receive a version that allocates
+/// its own vector for free.
 ///
 /// ```
 /// extern crate vox_box;
@@ -24,68 +42,57 @@ pub fn max_value<T: Float>(data: &[T]) -> T {
 /// assert_eq!(some_values.autocorrelate(2), vec![-1.0, -1.0]);
 /// ```
 pub trait Autocorrelate<T> 
-    where T: Zero +
-             Clone
+    where T: Sample
 {
     fn autocorrelate_mut(&self, n_coeffs: usize, coeffs: &mut [T]);
     fn autocorrelate(&self, n_coeffs: usize) -> Vec<T> {
-        let mut coeffs: Vec<T> = vec![T::zero(); n_coeffs];
+        let mut coeffs: Vec<T> = vec![T::equilibrium(); n_coeffs];
         self.autocorrelate_mut(n_coeffs, &mut coeffs);
         coeffs
     }
 }
 
-// Implement Autocorrelation for anything that can be indexed
-impl<V: ?Sized, T> Autocorrelate<T> for V
-    where T: Mul<T, Output=T> + 
-             Add<T, Output=T> + 
-             Div<T, Output=T> +
-             Zero +
-             Clone + 
-             PartialOrd,
-          V: Index<usize, Output=T> +
-             HasLength 
-{ 
+impl<T> Autocorrelate<T> for [T] 
+    where T: Sample
+{
     fn autocorrelate_mut(&self, n_coeffs: usize, coeffs: &mut [T]) {
         assert!(n_coeffs <= coeffs.len());
         for lag in 0..n_coeffs {
-            let mut accum = self[0].clone();
-            for i in 1..(self.len() - (lag)) {
-                accum = accum + (self[i].clone() * self[(i + lag) as usize].clone());
+            let mut accum: T = self[0];
+            for i in 1..(self.len() - lag) {
+                accum = accum.add_amp(self[i].mul_amp(self[(i + lag) as usize].to_float_sample()).to_signed_sample());
             }
             coeffs[lag] = accum;
         }
     }
 }
 
-#[derive(Clone, Debug)]
-pub struct Pitch<T> {
+#[derive(Clone, Copy, Debug)]
+pub struct Pitch<T: Float> {
     pub frequency: T,
     pub strength: T
 }
 
-impl<T> Pitch<T> {
+impl<T> Pitch<T> 
+    where T: Float
+{
     pub fn new(frequency: T, strength: T) -> Self {
         Pitch { frequency: frequency, strength: strength }
     }
 }
 
 pub struct PitchExtractor<'a, T: 'a + Float> {
-    frame_index: usize,
     voiced_unvoiced_cost: T,
     voicing_threshold: T,
-    candidates: &'a [&'a [Pitch<T>]],
-    current_cost: T
+    candidates: &'a [&'a [Pitch<T>]]
 }
 
 impl<'a, T: 'a + Float> PitchExtractor<'a, T> {
     pub fn new(candidates: &'a [&'a [Pitch<T>]], voiced_unvoiced_cost: T, voicing_threshold: T) -> Self {
         PitchExtractor {
-            frame_index: 0,
             voiced_unvoiced_cost: voiced_unvoiced_cost,
             voicing_threshold: voicing_threshold,
-            candidates: candidates,
-            current_cost: T::zero()
+            candidates: candidates
         }
     }
 }
@@ -94,40 +101,62 @@ impl<'a, T: 'a + Float> Iterator for PitchExtractor<'a, T> {
     type Item = Pitch<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.candidates.len() == self.frame_index {
-            return None;
+        let n_candidates = self.candidates.len();
+        if n_candidates == 0 { 
+            return None 
         }
 
-        self.frame_index += 1;
-        Some(self.candidates[self.frame_index][0].clone())
+        let candidate = self.candidates[0][0];
+        self.candidates = if n_candidates > 1 { 
+            &self.candidates[1..] 
+        } else { 
+            &[] 
+        };
+        Some(candidate)
     }
 }
 
-pub trait HasPitch<T> {
-    fn pitch(&self, sample_rate: T, threshold: T, silence_threshold: T, local_peak: T, global_peak: T, octave_cost: T, min: T, max: T, window_type: WindowType) -> Vec<Pitch<T>>;
-    fn local_maxima(&self) -> Vec<(usize, T)>;
+pub trait Pitched<S, T: Float> {
+    fn pitch<W: LagType>(&self, sample_rate: T, threshold: T, silence_threshold: S, local_peak: S, global_peak: S, octave_cost: T, min: T, max: T) -> Vec<Pitch<T>>;
 }
 
-impl<T: Float + PartialOrd + FromPrimitive> HasPitch<T> for Vec<T> {
+trait LocalMaxima<S: Sample> {
+    fn local_maxima(&self) -> Vec<(usize, S)>;
+}
+
+impl<S> LocalMaxima<S> for [S]
+    where S: Sample 
+{
+    /// Find the local maxima for a vector. Skips the one at index 0.
+    fn local_maxima(&self) -> Vec<(usize, S)> {
+        self.windows(3).enumerate().filter(|x| {
+            x.1[0] < x.1[1] && x.1[2] < x.1[1]
+        }).map(|x| ((x.0 + 1), x.1[1])).collect()
+    }
+}
+
+impl<S, T> Pitched<S, T> for [S]
+    where S: Sample, 
+          S::Float: ToPrimitive,
+          T: Float + FromPrimitive
+{
     /// Assumes that it's being passed a windowed signal
-    fn pitch(&self, sample_rate: T, threshold: T, silence_threshold: T, local_peak: T, global_peak: T, octave_cost: T, min: T, max: T, window_type: WindowType) -> Vec<Pitch<T>> {
-        let window_lag: Vec<T> = match window_type {
-            WindowType::Hanning => { Window::<T>::new(WindowType::HanningAutocorrelation, self.len()).collect() },
-            _ => { panic!() }
-        };
+    fn pitch<W: LagType>(&self, sample_rate: T, threshold: T, silence_threshold: S, local_peak: S, global_peak: S, octave_cost: T, min: T, max: T) -> Vec<Pitch<T>> {
+        let window_lag: Vec<S> = Window::<[S; 1], W>::new(self.len()).take(self.len()).map(|x| x.to_sample_slice()[0]).collect();
+
         let mut self_lag = self.autocorrelate(self.len());
         self_lag.normalize();
+
         for i in 0..self.len() {
-            self_lag[i] = self_lag[i] / window_lag[i];
+            self_lag[i] = (self_lag[i].to_float_sample() / window_lag[i].to_float_sample()).to_sample::<S>();
         };
 
-        let voiceless = (T::zero(), threshold + T::zero().max(T::from_i32(2).unwrap() - (local_peak / global_peak) / (silence_threshold / T::one() + threshold)));
+        let voiceless = (T::zero(), threshold + T::zero().max(T::one() + T::one() - (T::from(local_peak.to_float_sample()).unwrap() / T::from(global_peak.to_float_sample()).unwrap()) / (T::from(silence_threshold.to_float_sample()).unwrap() / T::one() + threshold)));
 
-        let local_max: Vec<(usize, T)> = self_lag.local_maxima();
-        let mut maxima: Vec<Pitch<T>> = local_max.iter()
+        let mut maxima: Vec<Pitch<T>> = self_lag.local_maxima().iter()
             .map(|x| {
                 let freq = sample_rate / T::from_usize(x.0).unwrap();
-                let mut strn = x.1 - (octave_cost.powi(2) * (min * freq).log2());
+                let mut strn = T::from(x.1.to_float_sample()).unwrap() - (octave_cost.powi(2) * (min * freq).log2());
                 if strn > T::from_f64(1.).unwrap() { strn = T::from_f64(1.).unwrap() / strn; }
                 Pitch { frequency: freq, strength: strn }
             })
@@ -136,13 +165,6 @@ impl<T: Float + PartialOrd + FromPrimitive> HasPitch<T> for Vec<T> {
         maxima.push(Pitch::new(T::from_usize(0).unwrap(), threshold)); // Index of 0 == no pitch
         maxima.sort_by(|a, b| (b.strength).partial_cmp(&a.strength).unwrap());
         maxima
-    }
-
-    // Find the local maxima for a vector. Skips the one at index 0.
-    fn local_maxima(&self) -> Vec<(usize, T)> {
-        self.windows(3).enumerate().filter(|x| {
-            x.1[0] < x.1[1] && x.1[2] < x.1[1]
-        }).map(|x| ((x.0 + 1), x.1[1])).collect()
     }
 }
 
--- src/waves.rs
+++ src/waves.rs
@@ -1,95 +1,25 @@
 extern crate num;
+extern crate sample;
 
 use std::f64::consts::PI;
 use std::iter::Iterator;
 use std::ops::*;
-use std::cmp::{PartialOrd};
 use std::cmp::Ordering::*;
+
 use num::Float;
-use num::traits::{FromPrimitive, ToPrimitive, Zero, One};
-use std::marker::PhantomData;
-use std::collections::VecDeque;
+use num::traits::{FromPrimitive, ToPrimitive};
 
-pub trait HasLength {
-    fn len(&self) -> usize;
-}
+use sample::Sample;
 
-impl<T> HasLength for [T] {
-    fn len(&self) -> usize {
-        self.len()
-    }
-}
-
-impl<'a, T> HasLength for &'a [T] {
-    fn len(&self) -> usize {
-        self.len()
-    }
-}
-
-impl<T> HasLength for Vec<T> {
-    fn len(&self) -> usize {
-        self.len()
-    }
-}
-
-impl<T> HasLength for VecDeque<T> {
-    fn len(&self) -> usize {
-        VecDeque::len(self)
-    }
-}
-
-pub trait Osc<T> {
-    fn sine(size: usize) -> Vec<T>;
-    fn saw(size: usize) -> Vec<T>;
-}
-
-impl<T: Float + FromPrimitive> Osc<T> for Vec<T> {
-    fn sine(size: usize) -> Vec<T> {
-        let mut sine: Vec<T> = Vec::with_capacity(size);
-        for i in 0..size {
-            let phase: T = T::from_usize(i).unwrap() * T::from_f32(2.0).unwrap() * T::from_f64(PI).unwrap() / T::from_f32(size as f32).unwrap();
-            sine.push(phase.sin());
-        }
-        sine
-    }
-
-    fn saw(size: usize) -> Vec<T> {
-        let mut saw: Vec<T> = Vec::with_capacity(size);
-        for i in 0..size {
-            let phase: T = T::from_f64((i as f64) / (size as f64)).unwrap();
-            saw.push((phase - T::from_f64(0.5f64).unwrap()) * T::from_f64(-2.0f64).unwrap());
-        }
-        saw
-    }
-}
-
-pub trait HasRMS<T> {
+pub trait RMS<T> {
     fn rms(&self) -> T;
 }
 
-impl<T: Float + FromPrimitive + ToPrimitive> HasRMS<T> for [T] {
+impl<T: Float + FromPrimitive + ToPrimitive> RMS<T> for [T] {
     fn rms(&self) -> T {
-        let sum = self.iter().fold(0f64, |acc, &item: &T| acc + item.powi(2).to_f64().unwrap());
+        let sum = self.iter()
+            .fold(0f64, |acc, &item: &T| acc + item.powi(2).to_f64().unwrap());
         T::from_f64((sum / self.len() as f64).sqrt()).unwrap()
-    }
-}
-
-pub trait Resample<T> {
-    fn resample_linear(self, usize) -> Self;
-}
-
-impl<T: Float + FromPrimitive> Resample<T> for Vec<T> {
-    fn resample_linear(self, new_size: usize) -> Self {
-        let mut resampled = Vec::<T>::with_capacity(new_size);
-        for i in 0..new_size {
-            let phase = (i as f64) / ((new_size-1) as f64);
-            let index = phase * ((self.len()-1) as f64);
-            let a = self[index.floor() as usize];
-            let b = self[index.ceil() as usize];
-            let t = T::from_f64(index - index.floor()).unwrap();
-            resampled.push(a + (b - a) * t);
-        }
-        resampled
     }
 }
 
@@ -97,187 +27,75 @@ pub trait Max<T> {
     fn max(&self) -> T;
 }
 
-impl<T: Copy + PartialOrd<T>> Max<T> for [T] {
+impl<T: Float> Max<T> for [T] {
     fn max(&self) -> T {
-        let mut max = self[0];
-        for i in 0..self.len() {
-            let elem = self[i];
-            max = match elem.partial_cmp(&max).unwrap_or(Equal) {
-                Less => { max }
-                Equal => { max }
-                Greater => { elem }
-            };
-        }
-        max
+        self.iter().fold(T::nan(), |acc, x| x.max(acc))
     }
 }
 
-pub trait Normalize<T> {
-    fn normalize(&mut self);
+pub trait Amplitude<S> {
+    fn amplitude(self) -> S;
 }
 
-impl<T: Float> Normalize<T> for [T] {
-    fn normalize(&mut self) {
-        let max = self.max();
-        for i in 0..self.len() {
-            self[i] = self[i] / max;
+impl<S: Sample> Amplitude<S> for S {
+    fn amplitude(self) -> S {
+        if self < S::equilibrium() {
+            self.mul_amp(S::Float::from_sample(-1.0))
+        } else {
+            self
         }
     }
 }
 
-/// Enum to select the type of window desired
-///
-/// { Hanning, Hamming }
-#[derive(Clone, Copy)]
-pub enum WindowType {
-    Rectangle,
-    Hanning,
-    HanningAutocorrelation,
-    Hamming
+pub trait MaxAmplitude<S> {
+    fn max_amplitude(&self) -> S;
 }
 
-/// Trait that defines something that can be windowed
-///
-/// window: should mutate self without allocating an additional window
-pub trait Windowable<T> {
-    fn window(&mut self, WindowType);
-}
-
-impl<T: Float + FromPrimitive> Windowable<T> for [T] {
-    fn window(&mut self, wtype: WindowType) {
-        let window = Window::<T>::new(wtype, self.len());
-        for idx in 0..self.len() {
-            self[idx] = self[idx] * window.val_at(idx);
-        }
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct Window<T> {
-    resource_type: PhantomData<T>,
-    window_type: WindowType,
-    len: usize,
-    idx: usize
-}
-
-impl<T> Window<T> {
-    pub fn new(window_type: WindowType, len: usize) -> Window<T> {
-        Window {
-            resource_type: PhantomData::<T>,
-            window_type: window_type,
-            len: len,
-            idx: 0
-        }
-    }
-}
-
-impl<T: Zero + One + Float + FromPrimitive> Window<T> {
-    fn val_at(&self, idx: usize) -> T {
-        match self.window_type {
-            WindowType::Rectangle => {
-                T::one()
-            },
-            WindowType::Hanning => {
-                let phase: f64 = (idx as f64 / (self.len as f64 - 1.)) * 2.0 * PI;
-                T::from_f64(0.5 * (1. - phase.cos())).unwrap_or(T::zero())
-            },
-            WindowType::HanningAutocorrelation => {
-                let phase: f64 = idx as f64 / (self.len as f64);
-                T::from_f64(
-                    ((1.0 - phase) *
-                    (2.0/3.0 + ((1.0/3.0) * (2.0 * PI * phase).cos()))) +
-                    (0.5 / PI) * (2.0 * PI * phase).sin()).unwrap_or(T::zero())
-            },
-            WindowType::Hamming => {
-                let phase: f64 = (idx as f64 / (self.len as f64 - 1.)) * 2.0 * PI;
-                T::from_f64(0.54 - (0.46 * phase.cos())).unwrap_or(T::zero())
+/// Returns the maximum peak amplitude in a given slice of samples
+impl<S: Sample> MaxAmplitude<S> for [S] {
+    fn max_amplitude(&self) -> S {
+        self[1..].iter().fold(self[0].amplitude(), |acc, elem| {
+            let amp = elem.amplitude();
+            match amp.partial_cmp(&acc) {
+                Some(Greater) => amp,
+                _ => acc
             }
-        }
+        })
     }
 }
 
-impl<T: Float + FromPrimitive> Iterator for Window<T> {
-    type Item = T;
-
-    fn next(&mut self) -> Option<T> {
-        if self.idx < self.len {
-            let ret = self.val_at(self.idx);
-            self.idx += 1;
-            Some(ret)
-        } else {
-            None
-        }
+pub trait Normalize<S> {
+    fn normalize_with_max(&mut self, max: Option<S>);
+    fn normalize(&mut self) {
+        self.normalize_with_max(None);
     }
 }
 
-/// Iterates over a block of data
-///
-/// Returns a Vec<T> at each iteration, which will be owned by the receiving context
-#[derive(Clone)]
-pub struct Windower<'a, T: 'a> {
-    window_type: WindowType,
-    hop_size: usize,
-    bin_size: usize,
-    current_index: usize,
-    data: &'a [T]
-}
-
-impl<'a, T> Windower<'a, T> {
-    pub fn new(window_type: WindowType, data: &'a [T], hop_size: usize, bin_size: usize) -> Windower<'a, T> {
-        Windower { 
-            window_type: window_type, 
-            hop_size: hop_size, 
-            bin_size: bin_size, 
-            current_index: 0,
-            data: data
-        }
-    }
-
-    pub fn len(&self) -> usize {
-        ((self.data.len() - self.bin_size) / self.hop_size) + 1
-    }
-
-    fn window(&self) -> Window<T> {
-        Window { 
-            resource_type: PhantomData::<T>, 
-            window_type: self.window_type,
-            len: self.bin_size,
-            idx: 0
-        }
-    }
-}
-
-impl<'a, T: Copy + Float + FromPrimitive> Iterator for Windower<'a, T> {
-    type Item = Vec<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current_index < (self.len() - 1) {
-            let start = self.current_index * self.hop_size;
-            let end = start + self.bin_size;
-            self.current_index += 1;
-            let window = self.window();
-            let data_iter = self.data[start..end].iter();
-
-            let out = window.zip(data_iter).map(|val| {
-                val.0 * *val.1
-            }).collect();
-
-            Some(out)
-        } else {
-            None
+impl<S: Sample> Normalize<S> for [S] {
+    fn normalize_with_max(&mut self, max: Option<S>) {
+        let scale_factor: <S as Sample>::Float = <S as Sample>::Float::identity() / 
+            max.unwrap_or(self.max_amplitude()).to_float_sample();
+        for elem in self.iter_mut() {
+            *elem = elem.mul_amp(scale_factor);
         }
     }
 }
 
 /// Filter
 ///
-/// Preemphasis gives a 6db/oct boost above a particular center frequency
+/// Preemphasis should give a 6db/oct boost above a particular center frequency
 /// Factor is center frequency / sample_rate
 pub trait Filter<T> {
     fn preemphasis(&mut self, factor: T) -> &mut [T];
 }
 
-impl<T> Filter<T> for [T] where T: Mul<T, Output=T> + Sub<T, Output=T> + Copy + ToPrimitive + FromPrimitive {
+impl<T> Filter<T> for [T] 
+    where T: Mul<T, Output=T> + 
+             Sub<T, Output=T> + 
+             Copy + 
+             ToPrimitive + 
+             FromPrimitive 
+{
     fn preemphasis<'a>(&'a mut self, factor: T) -> &'a mut [T] {
         let filter = T::from_f64((-2.0 * PI * factor.to_f64().unwrap()).exp()).unwrap();
         for i in (1..self.len()).rev() {
@@ -293,54 +111,59 @@ mod tests {
     use super::super::*;
     use super::super::periodic::*;
 
+    use sample::conv::ToSampleSlice;
+
+    fn sine(len: usize) -> Vec<f64> {
+        let rate = sample::signal::rate(len).const_hz(1.0);
+        rate.clone().sine().take(len).collect::<Vec<[f64; 1]>>().to_sample_slice()
+    }
+
     #[test]
     fn test_pe() {
-        let mut saw = Vec::<f64>::saw(32);
-        let mut sine = Vec::<f64>::sine(32);
-        saw.preemphasis(0.1f64); // preemphasize at 0.1 * sampling rate
+        let mut sine = sine(32);
         sine.preemphasis(0.1f64); // preemphasize at 0.1 * sampling rate
     }
 
-    #[test]
-    fn test_windower() {
-        let data = Vec::<f64>::sine(64);
-        let windower = Windower::new(WindowType::Hanning, &data[..], 16, 32);
-        assert_eq!(windower.len(), 3);
-    }
+    // #[test]
+    // fn test_windower() {
+    //     let data = Vec::<f64>::sine(64);
+    //     let windower = Windower::new(WindowType::Hanning, &data[..], 16, 32);
+    //     assert_eq!(windower.len(), 3);
+    // }
 
-    #[test]
-    fn test_window_autocorr() {
-        let data: Vec<f64> = Window::<f64>::new(WindowType::HanningAutocorrelation, 16).collect();
-        println!("window autocorr: {:?}", &data);
-        let mut manual: Vec<f64> = Window::<f64>::new(WindowType::Hanning, 16).collect();
-        manual = manual.autocorrelate(16);
-        manual.normalize();
-        println!("manual autocorr: {:?}", &manual);
-        for i in 0..16 {
-            let diff = (manual[i] - data[i]).abs();
-            assert!(diff < 1e-1);
-        }
-    }
+    // #[test]
+    // fn test_window_autocorr() {
+    //     let data: Vec<f64> = Window::<f64>::new(WindowType::HanningAutocorrelation, 16).collect();
+    //     println!("window autocorr: {:?}", &data);
+    //     let mut manual: Vec<f64> = Window::<f64>::new(WindowType::Hanning, 16).collect();
+    //     manual = manual.autocorrelate(16);
+    //     manual.normalize();
+    //     println!("manual autocorr: {:?}", &manual);
+    //     for i in 0..16 {
+    //         let diff = (manual[i] - data[i]).abs();
+    //         assert!(diff < 1e-1);
+    //     }
+    // }
 
-    #[test]
-    fn test_resample_vec32() {
-        let upsamp: Vec<f32> = vec![1f32, 2.0, 3.0, 4.0];
-        let exp: Vec<f32> = vec![1f32, 4.0];
-        let res: Vec<f32> = upsamp.resample_linear(2);
-        assert_eq!(res, exp);
-    }
+    // #[test]
+    // fn test_resample_vec32() {
+    //     let upsamp: Vec<f32> = vec![1f32, 2.0, 3.0, 4.0];
+    //     let exp: Vec<f32> = vec![1f32, 4.0];
+    //     let res: Vec<f32> = upsamp.resample_linear(2);
+    //     assert_eq!(res, exp);
+    // }
 
-    #[test]
-    fn test_resample_vec64() {
-        let upsamp: Vec<f64> = vec![1f64, 2.0, 3.0, 4.0];
-        let exp: Vec<f64> = vec![1f64, 2.5, 4.0];
-        let res: Vec<f64> = upsamp.resample_linear(3);
-        assert_eq!(res, exp);
-    }
+    // #[test]
+    // fn test_resample_vec64() {
+    //     let upsamp: Vec<f64> = vec![1f64, 2.0, 3.0, 4.0];
+    //     let exp: Vec<f64> = vec![1f64, 2.5, 4.0];
+    //     let res: Vec<f64> = upsamp.resample_linear(3);
+    //     assert_eq!(res, exp);
+    // }
 
     #[test]
     fn test_rms() {
-        let sine = Vec::<f64>::sine(64);
+        let sine = sine(64);
         let rms = sine.rms();
         println!("rms is {:?}", rms);
         assert!((rms - 0.707).abs() < 0.001);
